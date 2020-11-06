fff458a48babd62f84de6c7d7bcf6f92ca5f1689
Eric Cornelissen <ericornelissen@gmail.com>
2020-11-06 10:33:27 +0100

    Run format
    Run `npm run format` to update the formatting according to the new
    Prettier version. Also, running `npm run format` will now format .yml
    files as well.

--- test/index.test.js
+++ test/index.test.js
@@ -2,20 +2,20 @@ const assert = require("assert");
 
 const shescape = require("../index.js");
 
-it("should return the input if nothing has to be escaped", function() {
+it("should return the input if nothing has to be escaped", function () {
   const input = "Hello world!";
   const output = shescape(input);
   assert.strictEqual(output, input);
 });
 
-describe("escape single quotes", function() {
-  it("one single quote", function() {
+describe("escape single quotes", function () {
+  it("one single quote", function () {
     const input = "' & ls -al";
     const output = shescape(input);
     assert.strictEqual(output, "'\\'' & ls -al");
   });
 
-  it("two single quotes", function() {
+  it("two single quotes", function () {
     const input = "' & echo 'Hello world!'";
     const output = shescape(input);
     assert.strictEqual(output, "'\\'' & echo '\\''Hello world!'\\''");
