3515b6ecec479849a1b8f99fad3e9f1c6cdf1071
Eric Cornelissen <ericornelissen@gmail.com>
2020-11-06 09:37:35 +0100

    Set up CI and expand tests
    Set up Continuous Integration using GitHub Actions for this project.
    The CI is currently configured to run all test suites and lint the code
    base.

--- test/index.test.js
+++ test/index.test.js
@@ -2,8 +2,22 @@ const assert = require("assert");
 
 const shescape = require("../index.js");
 
-it("should return input if nothing has to be escaped", function() {
+it("should return the input if nothing has to be escaped", function() {
   const input = "Hello world!";
   const output = shescape(input);
   assert.strictEqual(output, input);
+});
+
+describe("escape single quotes", function() {
+  it("one single quote", function() {
+    const input = "' & ls -al";
+    const output = shescape(input);
+    assert.strictEqual(output, "'\\'' & ls -al");
+  });
+
+  it("two single quotes", function() {
+    const input = "' & echo 'Hello world!'";
+    const output = shescape(input);
+    assert.strictEqual(output, "'\\'' & echo '\\''Hello world!'\\''");
+  });
 });
