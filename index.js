0d851d41b4fc96652110e14627d48a5a34090f9f
Eric Cornelissen <ericornelissen@gmail.com>
2020-12-08 01:35:48 +0100

    Remove direct call to shescape

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const os = require("os");
 
 const main = require("./src/main.js");
 
-module.exports = function (arg) {
-  console.warn(
-    "calling shescape() directly is deprecated since v0.3.1 and will be removed in v1.0.0"
-  );
-  const platform = os.platform();
-  return main.escapeShellArgByPlatform(arg, platform);
-};
-
 module.exports.escape = function (arg) {
   const platform = os.platform();
   return main.escapeShellArgByPlatform(arg, platform);
