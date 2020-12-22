b4e5f83b7d7efa8af29a56db2c922cfc557936cd
Eric Cornelissen <ericornelissen@gmail.com>
2020-12-22 18:18:48 +0100

    Add escapeAll function
    Add an escape-variant of the quoteAll function and test it in
    index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -23,6 +23,46 @@ describe("index.js", function () {
     main.escapeShellArgByPlatform.restore();
   });
 
+  it("escapeAll calls main for every value", function () {
+    const osStubOutput = "MundOS";
+    sinon.stub(os, "platform").returns(osStubOutput);
+    const escapeStub = sinon.stub(main, "escapeShellArgByPlatform");
+
+    const input1 = "foo'";
+    const input2 = "'bar";
+    const output1 = "foo''";
+    const output2 = "''bar";
+
+    escapeStub.withArgs(input1).returns(output1);
+    escapeStub.withArgs(input2).returns(output2);
+
+    const inputs = [input1, input2];
+    const output = shescape.escapeAll(inputs);
+    assert(os.platform.called);
+    assert(main.escapeShellArgByPlatform.called);
+    assert(main.escapeShellArgByPlatform.calledWith(input1, osStubOutput));
+    assert(main.escapeShellArgByPlatform.calledWith(input2, osStubOutput));
+    assert.deepStrictEqual(output, [output1, output2]);
+
+    os.platform.restore();
+    main.escapeShellArgByPlatform.restore();
+  });
+
+  it("escapeAll gracefully handles inputs that are not an array", function () {
+    const osStubOutput = "MundOS";
+    sinon.stub(os, "platform").returns(osStubOutput);
+    const mainStubOutput = "foobar";
+    sinon.stub(main, "escapeShellArgByPlatform").returns(mainStubOutput);
+
+    const output = shescape.escapeAll("Hello world!");
+    assert(os.platform.called);
+    assert(main.escapeShellArgByPlatform.called);
+    assert.deepStrictEqual(output, [mainStubOutput]);
+
+    os.platform.restore();
+    main.escapeShellArgByPlatform.restore();
+  });
+
   it("quote calls main for current OS", function () {
     const osStubOutput = "MundOS";
     sinon.stub(os, "platform").returns(osStubOutput);
