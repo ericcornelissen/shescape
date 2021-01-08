f89a38736fd8ddfaba9a2daaacef752a18554b27
Eric Cornelissen <ericornelissen@gmail.com>
2021-01-08 21:27:29 +0100

    Add in-source JSDoc documentation to all .js files

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,16 @@
+/**
+ * @overview Contains constants that may be used in multiple modules.
+ * @license MPL-2.0
+ * @author Eric Cornelissen <ericornelissen@gmail.com>
+ */
+
+/**
+ * @constant {string} typeError The error message for incorrect parameter types.
+ */
 module.exports.typeError =
   "Shescape requires strings or values that can be converted into a string using .toString()";
+
+/**
+ * @constant {string} win32 The string identifying Windows systems.
+ */
 module.exports.win32 = "win32";
--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+/**
+ * @overview Contains functionality to escape and quote shell arguments on any
+ * operating system.
+ * @license MPL-2.0
+ * @author Eric Cornelissen <ericornelissen@gmail.com>
+ */
+
 const { typeError, win32 } = require("./constants.js");
 const unix = require("./unix.js");
 const win = require("./win.js");
 
+/**
+ * Check if a value can be converted into a string.
+ *
+ * @param {any} value The value of interest.
+ * @returns {boolean} `true` iff `value` can be converted into a string.
+ */
 function isStringable(value) {
   if (value === undefined || value === null) {
     return false;
@@ -10,6 +23,16 @@ function isStringable(value) {
   return typeof value.toString === "function";
 }
 
+/**
+ * Take a value and escape any dangerous characters.
+ *
+ * Non-string inputs will be converted to strings using a `toString()` method.
+ *
+ * @param {string} arg The argument to escape.
+ * @param {string} platform The platform to escape the argument for.
+ * @returns {string} The escaped argument.
+ * @throws {TypeError} The argument is not stringable.
+ */
 function escapeShellArgByPlatform(arg, platform) {
   let argAsString = arg;
   if (typeof arg !== "string") {
@@ -28,6 +51,17 @@ function escapeShellArgByPlatform(arg, platform) {
   }
 }
 
+/**
+ * Take a value, put OS-specific quotes around it, and escape any dangerous
+ * characters.
+ *
+ * Non-string inputs will be converted to strings using a `toString()` method.
+ *
+ * @param {string} arg The argument to escape and quote.
+ * @param {string} platform The platform to escape and quote the argument for.
+ * @returns {string} The escaped argument.
+ * @throws {TypeError} The argument is not stringable.
+ */
 function quoteByPlatform(arg, platform) {
   const safeArg = escapeShellArgByPlatform(arg, platform);
   switch (platform) {
