c1ece6d36403f951ac27f782c7449b1538aa68a3
Eric Cornelissen <ericornelissen@gmail.com>
2021-11-27 10:27:46 +0100

    Improve test suite for transpiled code
    Add smoke-like tests+assertions to the test suite for the transpiled
    version of the library to increase confidence that it works as expected.

--- test/index.test.cjs
+++ test/index.test.cjs
@@ -15,6 +15,11 @@ describe("index.cjs", function () {
 
     const escapeIsFunction = typeof shescape.escape === "function";
     assert.ok(escapeIsFunction);
+
+    const input = "Hello world!";
+    const result = shescape.escape(input);
+    assert.notStrictEqual(result, "");
+    assert.doesNotMatch(result, /(?<q>"|').*?(\k<q>)/);
   });
 
   it("has an `escapeAll` function", function () {
@@ -23,6 +28,13 @@ describe("index.cjs", function () {
 
     const escapeAllIsFunction = typeof shescape.escapeAll === "function";
     assert.ok(escapeAllIsFunction);
+
+    const inputs = ["foo", "bar"];
+    const result = shescape.escapeAll(inputs);
+    for (const output of result) {
+      assert.notStrictEqual(output, "");
+      assert.doesNotMatch(output, /(?<q>"|').*?(\k<q>)/);
+    }
   });
 
   it("has an `quote` function", function () {
@@ -31,6 +43,11 @@ describe("index.cjs", function () {
 
     const quoteIsFunction = typeof shescape.quote === "function";
     assert.ok(quoteIsFunction);
+
+    const input = "Hello world!";
+    const result = shescape.quote(input);
+    assert.notStrictEqual(result, "");
+    assert.match(result, /(?<q>"|').*?(\k<q>)/);
   });
 
   it("has an `quoteAll` function", function () {
@@ -39,5 +56,12 @@ describe("index.cjs", function () {
 
     const quoteAllIsFunction = typeof shescape.quoteAll === "function";
     assert.ok(quoteAllIsFunction);
+
+    const inputs = ["foo", "bar"];
+    const result = shescape.quoteAll(inputs);
+    for (const output of result) {
+      assert.notStrictEqual(output, "");
+      assert.match(output, /(?<q>"|').*?(\k<q>)/);
+    }
   });
 });
