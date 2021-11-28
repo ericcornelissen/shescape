ee9080b877c1a168d4ffbf9b86aa7208eda84f74
Eric Cornelissen <ericornelissen@gmail.com>
2021-11-28 18:06:48 +0100

    Use local echo helper for fuzzing
    Update the fuzzer to use a project-local echo-like helper, written in
    JavaScript for Node.js, as an oracle to verify that shescape escapes
    arguments properly. The echo utility simply reads the first argument it
    receives when called like
    
      node path/to/echo.js "first argument"
    
    and writes it back to std out.
    
    This change would have a couple of advantages over the previous approach
    1. It removes the `printf` prerequisite, which by extension ...
    2. Enables fuzzing on Windows (untested as of creating this commit), and
    3. Avoids having to account for `printf` behaviour in the fuzz target.

--- test/index.fuzz.cjs
+++ test/index.fuzz.cjs
@@ -7,33 +7,12 @@
 const cp = require("child_process");
 const shescape = require("../index.cjs");
 
-function prepareArgForPrintf(arg) {
-  let result = arg
-    .replace(/[\n\r]+/g, " ") // avoid dealing with newlines
-    .replace(/^(\u{0}*)(-\u{0}*)+/gu, "$1") // avoid dealing with leading "-"
-    .replace(/(?<!%)(%%)*%(?!%)/g, "%%%%") // avoid dealing with printf formatting
-    .replace(/(?<![^\\](\\\\)*\\)\\(\u{0}*[0-9]){1,3}/gu, " "); // avoid dealing with "\NNN"
-
-  if (result === arg) {
-    return result;
-  } else {
-    return prepareArgForPrintf(result);
-  }
+function prepareArg(arg) {
+  return arg.replace(/[\n\r]+/g, ""); // avoid dealing with newlines
 }
 
-function getExpectedPrintfString(arg) {
-  return arg
-    .replace(/\u{0}/gu, "") // Remove null characters
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\a/g, String.fromCharCode(7)) // Replace "\a  alert (BEL)"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\b/g, String.fromCharCode(8)) // Replace "\b  backspace"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\e/g, String.fromCharCode(27)) // Replace "\e  escape"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\f/g, String.fromCharCode(12)) // Replace "\f  form feed"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\n/g, String.fromCharCode(10)) // Replace "\n  new return"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\r/g, String.fromCharCode(13)) // Replace "\r  carriage return"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\t/g, String.fromCharCode(9)) // Replace "\t  horizontal tab"
-    .replace(/(?<!(^|[^\\])(\\\\)*\\)\\v/g, String.fromCharCode(11)) // Replace "\v  vertical tab"
-    .replace(/\\\\/g, "\\") // Unescape \
-    .replace(/%%/g, "%"); // Unescape %
+function getExpectedOutput(arg) {
+  return arg.replace(/\u{0}/gu, ""); // Remove null characters
 }
 
 function checkIfAnyApiFunctionThrows(arg) {
@@ -49,10 +28,10 @@ function checkIfAnyApiFunctionThrows(arg) {
 }
 
 function checkQuotesAndEscapesCorrectly(arg) {
-  const preparedArg = prepareArgForPrintf(arg);
+  const preparedArg = prepareArg(arg);
   const quotedArg = shescape.quote(preparedArg);
-  const result = cp.execSync(`printf ${quotedArg}`).toString();
-  const expected = getExpectedPrintfString(preparedArg);
+  const result = cp.execSync(`node test/fuzz/echo.js ${quotedArg}`).toString();
+  const expected = getExpectedOutput(preparedArg);
   if (expected !== result) {
     throw new Error(
       "Unexpected output (- got, + expected):\n" +
