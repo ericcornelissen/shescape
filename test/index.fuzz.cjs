9011f7e8b5e3d744937f1179239cf57d330f4d80
Eric Cornelissen <ericornelissen@gmail.com>
2021-11-29 21:53:43 +0100

    Fix fuzzing crash on Windows
    Command arguments for Node.js on Windows are interpreted with
    backslashes escaping double quotes (as if it's a JavaScript string). As
    a result, arguments echod by the `test/fuzz/echo.js` script may have
    characters missing. The fuzz target is updated to deal with this by
    preparing the argument to be escaped.

--- test/index.fuzz.cjs
+++ test/index.fuzz.cjs
@@ -5,14 +5,23 @@
  */
 
 const cp = require("child_process");
+const os = require("os");
+
 const shescape = require("../index.cjs");
 
 function prepareArg(arg) {
-  return arg.replace(/[\n\r]+/g, ""); // avoid dealing with newlines
+  let result = arg.replace(/[\n\r]+/g, ""); // Avoid dealing with newlines
+  if (os.platform() == "win32") {
+    result = result.replace(/((\\\u{0}*)+)(?=\u{0}*("|$))/gu, "$1$1");
+  }
+
+  return result;
 }
 
 function getExpectedOutput(arg) {
-  return arg.replace(/\u{0}/gu, ""); // Remove null characters
+  return arg
+    .replace(/[\n\r]+/g, "") // Avoid dealing with newlines
+    .replace(/\u{0}/gu, ""); // Remove null characters
 }
 
 function checkIfAnyApiFunctionThrows(arg) {
@@ -31,7 +40,7 @@ function checkQuotesAndEscapesCorrectly(arg) {
   const preparedArg = prepareArg(arg);
   const quotedArg = shescape.quote(preparedArg);
   const result = cp.execSync(`node test/fuzz/echo.js ${quotedArg}`).toString();
-  const expected = getExpectedOutput(preparedArg);
+  const expected = getExpectedOutput(arg);
   if (expected !== result) {
     throw new Error(
       "Unexpected output (- got, + expected):\n" +
