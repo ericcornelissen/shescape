f45c5139f2bdf50632dda6c53b41a41b198fd3f1
Eric Cornelissen <ericornelissen@gmail.com>
2021-12-19 14:50:22 +0100

    Simplify and improve index.test.cjs
    - Fix file overview
    - Simplify test assertions
    - Update individual test descriptions
    - Improve regular expression used for asserting quote functions.

--- test/index.test.cjs
+++ test/index.test.cjs
@@ -1,5 +1,5 @@
 /**
- * @overview Contains unit tests for `./index.cjs`.
+ * @overview Contains smoke tests for `./index.cjs`.
  * @license Unlicense
  * @author Eric Cornelissen <ericornelissen@gmail.com>
  */
@@ -9,59 +9,47 @@ const assert = require("assert");
 const shescape = require("../index.cjs");
 
 describe("index.cjs", function () {
-  it("has an `escape` function", function () {
-    const hasEscapeProperty = shescape.hasOwnProperty("escape");
-    assert.ok(hasEscapeProperty);
-
-    const escapeIsFunction = typeof shescape.escape === "function";
-    assert.ok(escapeIsFunction);
+  it("has a functioning `escape` function", function () {
+    assert.ok(shescape.hasOwnProperty("escape"));
+    assert.ok(typeof shescape.escape === "function");
 
     const input = "Hello world!";
     const result = shescape.escape(input);
     assert.notStrictEqual(result, "");
-    assert.doesNotMatch(result, /(?<q>"|').*?(\k<q>)/);
+    assert.doesNotMatch(result, /^(?<q>"|').*(\k<q>)$/);
   });
 
-  it("has an `escapeAll` function", function () {
-    const hasEscapeAllProperty = shescape.hasOwnProperty("escapeAll");
-    assert.ok(hasEscapeAllProperty);
-
-    const escapeAllIsFunction = typeof shescape.escapeAll === "function";
-    assert.ok(escapeAllIsFunction);
+  it("has a functioning `escapeAll` function", function () {
+    assert.ok(shescape.hasOwnProperty("escapeAll"));
+    assert.ok(typeof shescape.escapeAll === "function");
 
     const inputs = ["foo", "bar"];
     const result = shescape.escapeAll(inputs);
     for (const output of result) {
       assert.notStrictEqual(output, "");
-      assert.doesNotMatch(output, /(?<q>"|').*?(\k<q>)/);
+      assert.doesNotMatch(output, /^(?<q>"|').*(\k<q>)$/);
     }
   });
 
-  it("has an `quote` function", function () {
-    const hasQuoteProperty = shescape.hasOwnProperty("quote");
-    assert.ok(hasQuoteProperty);
-
-    const quoteIsFunction = typeof shescape.quote === "function";
-    assert.ok(quoteIsFunction);
+  it("has a functioning `quote` function", function () {
+    assert.ok(shescape.hasOwnProperty("quote"));
+    assert.ok(typeof shescape.quote === "function");
 
     const input = "Hello world!";
     const result = shescape.quote(input);
     assert.notStrictEqual(result, "");
-    assert.match(result, /(?<q>"|').*?(\k<q>)/);
+    assert.match(result, /^(?<q>"|').*(\k<q>)$/);
   });
 
-  it("has an `quoteAll` function", function () {
-    const hasQuoteAllProperty = shescape.hasOwnProperty("quoteAll");
-    assert.ok(hasQuoteAllProperty);
-
-    const quoteAllIsFunction = typeof shescape.quoteAll === "function";
-    assert.ok(quoteAllIsFunction);
+  it("has a functioning `quoteAll` function", function () {
+    assert.ok(shescape.hasOwnProperty("quoteAll"));
+    assert.ok(typeof shescape.quoteAll === "function");
 
     const inputs = ["foo", "bar"];
     const result = shescape.quoteAll(inputs);
     for (const output of result) {
       assert.notStrictEqual(output, "");
-      assert.match(output, /(?<q>"|').*?(\k<q>)/);
+      assert.match(output, /^(?<q>"|').*(\k<q>)$/);
     }
   });
 });
