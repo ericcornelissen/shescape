d383c6254a38cad378210d4ddbd42d0c5d0f42a0
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-24 23:19:12 +0100

    Refactor benchmark code for clarity
    - Re-order statements
    - Rewrite to resemble tests more closely (with one `suite.add` per line)
    - Name the benchmark suite to avoid reuse.
    - Use values from `test/common.js`.

--- test/bench.js
+++ test/bench.js
@@ -1,23 +1,34 @@
 import Benchmark from "benchmark";
 
-const suite = new Benchmark.Suite();
+import { binSh, binBash, cmdExe, powershellExe } from "./common.js";
 
 import * as unix from "../src/unix.js";
 import * as win from "../src/win.js";
 
-const arg = "foobar";
+const sampleArg = "foobar";
 
-suite
-  .add("unix::escapeShellArg(., .)", () => {
-    unix.escapeShellArg(arg, "/bin/sh");
-  })
-  .add('win::escapeShellArg(., "cmd.exe")', () => {
-    win.escapeShellArg(arg, "cmd.exe");
-  })
-  .add('win::escapeShellArg(., "powershell.exe")', () => {
-    win.escapeShellArg(arg, "powershell.exe");
-  })
-  .on("cycle", (event) => {
-    console.log(`${event.target}`);
-  })
-  .run();
+const suite = new Benchmark.Suite("escapeShellArg", {
+  onCycle: (event) => {
+    const fn = event.currentTarget.name;
+    const cycleResult = event.target.toString();
+    console.log(fn, "-", cycleResult);
+  },
+});
+
+suite.add(`unix, ${binSh}, ${sampleArg}`, () => {
+  unix.escapeShellArg(sampleArg, binSh);
+});
+
+suite.add(`unix, ${binBash}, ${sampleArg}`, () => {
+  unix.escapeShellArg(sampleArg, binBash);
+});
+
+suite.add(`win, ${cmdExe}, ${sampleArg}`, () => {
+  win.escapeShellArg(sampleArg, cmdExe);
+});
+
+suite.add(`win, ${powershellExe}, ${sampleArg}`, () => {
+  win.escapeShellArg(sampleArg, powershellExe);
+});
+
+suite.run();
