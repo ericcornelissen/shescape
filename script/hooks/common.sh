1a6dbb429bae78e671483a0525411e7ef6f21cd2
Eric Cornelissen <ericornelissen@gmail.com>
2022-01-29 23:57:41 +0100

    Use `if []; then` syntax over `if []\nthen` for succinctness

--- script/hooks/common.sh
+++ script/hooks/common.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 IS_MERGING=""  # false
-if [ -f "$(git rev-parse --git-dir)/MERGE_HEAD" ]
-then
+if [ -f "$(git rev-parse --git-dir)/MERGE_HEAD" ]; then
   IS_MERGING="x"  # true
 fi
 
 get_stash_count () {
   local count=$(git rev-list --walk-reflogs --count refs/stash 2> /dev/null)
-  if [ "$count" = "" ]
-  then
+  if [ "$count" = "" ]; then
     echo "0"
   else
     echo $count
