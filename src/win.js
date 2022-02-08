e46cca9953aafd9ce478ced5ce834a4740de552e
Eric Cornelissen <ericornelissen@gmail.com>
2022-02-08 09:30:33 +0100

    Escape "," for Windows PowerShell with interpolation

--- src/win.js
+++ src/win.js
@@ -48,7 +48,7 @@ function escapeShellArgsForPowerShell(arg, interpolation) {
       .replace(/^@/g, "`@")
       .replace(/^\]/g, "`]")
       .replace(/^#/g, "`#")
-      .replace(/(\;|\&|\|)/g, "`$1")
+      .replace(/(,|\;|\&|\|)/g, "`$1")
       .replace(/(\(|\)|\{|\})/g, "`$1")
       .replace(/('|’|‘|‛|‚)/g, "`$1")
       .replace(/("|“|”|„)/g, "`$1");
