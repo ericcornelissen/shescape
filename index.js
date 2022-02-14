07fd3b225c7c36917a4a5ac49afa12b9e2edfc50
ericcornelissen <ericcornelissen@users.noreply.github.com>
2022-02-14 23:17:41 +0000

    Version bump

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
  *   cp.spawn("command", shescape.escapeAll(userInput), options);
  *
  * @module shescape
- * @version 1.4.0
+ * @version 1.5.0
  * @license MPL-2.0
  * @author Eric Cornelissen <ericornelissen@gmail.com>
  */
