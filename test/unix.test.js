8f5dd1c8bd38e302676ec451bc06afd978f5859a
Eric Cornelissen <ericornelissen@gmail.com>
2022-02-19 19:02:35 +0100

    Test escaping of `=~` for Bash-like shells

--- test/unix.test.js
+++ test/unix.test.js
@@ -440,8 +440,20 @@ describe("unix.js", function () {
             assert.strictEqual(output, "\\~foobar");
           });
 
-          it("does nothing to a tilde not at the start", function () {
+          it("escapes a tilde after '=' at the end", function () {
+            const input = "foobar=~";
+            const output = escapeShellArg(input, shell, interpolation);
+            assert.strictEqual(output, "foobar=\\~");
+          });
+
+          it("does nothing to a tilde in the middle", function () {
             const input = "foo~bar";
+            const output = escapeShellArg(input, shell, interpolation);
+            assert.strictEqual(output, input);
+          });
+
+          it("does nothing to a tilde after '=' in the middle", function () {
+            const input = "foo=~bar";
             const output = escapeShellArg(input, shell, interpolation);
             assert.strictEqual(output, input);
           });
@@ -450,6 +462,12 @@ describe("unix.js", function () {
             const input = "~foo~bar";
             const output = escapeShellArg(input, shell, interpolation);
             assert.strictEqual(output, "\\~foo~bar");
+          });
+
+          it("only escapes the tilde after '=' at the end", function () {
+            const input = "foo~bar=~";
+            const output = escapeShellArg(input, shell, interpolation);
+            assert.strictEqual(output, "foo~bar=\\~");
           });
         });
 
