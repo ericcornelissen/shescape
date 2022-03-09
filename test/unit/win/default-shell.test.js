bda52d13e0d7a89173a31172c877cc13966e2ab6
Eric Cornelissen <ericornelissen@gmail.com>
2022-03-09 23:28:17 +0100

    Add initial AVA test suite

---
+++ test/unit/win/default-shell.test.js
@@ -0,0 +1,35 @@
+/**
+ * @overview Contains unit tests for the `getDefaultShell` function in
+ * `./src/win.js`.
+ * @license Unlicense
+ * @author Eric Cornelissen <ericornelissen@gmail.com>
+ */
+
+import test from "ava";
+
+import { binCmd } from "../../common.js";
+
+import * as win from "../../../src/win.js";
+
+test("returns the value of %COMSPEC%", (t) => {
+  const ComSpec = "C:\\Windows\\System32\\cmd.exe";
+  const env = { ComSpec };
+
+  const result = win.getDefaultShell({ env });
+  t.is(result, ComSpec);
+});
+
+test("returns the value of %COMSPEC% when it's an empty string", (t) => {
+  const ComSpec = "";
+  const env = { ComSpec };
+
+  const result = win.getDefaultShell({ env });
+  t.is(result, ComSpec);
+});
+
+test("returns 'cmd.exe' if %COMSPEC% is not defined", (t) => {
+  const env = {};
+
+  const result = win.getDefaultShell({ env });
+  t.is(result, binCmd);
+});
