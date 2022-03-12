6b8867baf1edac469186ef2700c441b87287215d
Eric Cornelissen <ericornelissen@gmail.com>
2022-03-12 14:35:41 +0100

    Add AVA-based unit tests for Zsh escaping

--- test/unit/unix/get-escape-function.test.js
+++ test/unit/unix/get-escape-function.test.js
@@ -7,7 +7,7 @@
 
 import test from "ava";
 
-import { binBash, binDash } from "../../common.js";
+import { binBash, binDash, binZsh } from "../../common.js";
 import * as macros from "../macros.js";
 
 import * as unix from "../../../src/unix.js";
@@ -734,6 +734,302 @@ const tests = {
       {
         input: "a{0..2}b",
         expected: { interpolation: "a\\{0..2}b", noInterpolation: "a{0..2}b" },
+      },
+    ],
+    "angle brackets ('<', '>')": [
+      {
+        input: "a<b",
+        expected: { interpolation: "a\\<b", noInterpolation: "a<b" },
+      },
+      {
+        input: "a>b",
+        expected: { interpolation: "a\\>b", noInterpolation: "a>b" },
+      },
+      {
+        input: "a<b<c",
+        expected: { interpolation: "a\\<b\\<c", noInterpolation: "a<b<c" },
+      },
+      {
+        input: "a>b>c",
+        expected: { interpolation: "a\\>b\\>c", noInterpolation: "a>b>c" },
+      },
+      {
+        input: "a<b>c",
+        expected: { interpolation: "a\\<b\\>c", noInterpolation: "a<b>c" },
+      },
+    ],
+  },
+  [binZsh]: {
+    "sample strings": [
+      {
+        input: "foobar",
+        expected: { interpolation: "foobar", noInterpolation: "foobar" },
+      },
+      {
+        input: "Hello world",
+        expected: {
+          interpolation: "Hello world",
+          noInterpolation: "Hello world",
+        },
+      },
+    ],
+    "null characters (\x00)": [
+      {
+        input: "a\x00b",
+        expected: { interpolation: "ab", noInterpolation: "ab" },
+      },
+      {
+        input: "a\x00b\x00c",
+        expected: { interpolation: "abc", noInterpolation: "abc" },
+      },
+    ],
+    'single quotes ("\'")': [
+      {
+        input: "a'b",
+        expected: { interpolation: "a\\'b", noInterpolation: "a'\\''b" },
+      },
+      {
+        input: "a'b'c",
+        expected: {
+          interpolation: "a\\'b\\'c",
+          noInterpolation: "a'\\''b'\\''c",
+        },
+      },
+    ],
+    "double quotes ('\"')": [
+      {
+        input: 'a"b',
+        expected: { interpolation: 'a\\"b', noInterpolation: 'a"b' },
+      },
+      {
+        input: 'a"b"c',
+        expected: { interpolation: 'a\\"b\\"c', noInterpolation: 'a"b"c' },
+      },
+    ],
+    "backticks (')": [
+      {
+        input: "a`b",
+        expected: { interpolation: "a\\`b", noInterpolation: "a`b" },
+      },
+      {
+        input: "a`b`c",
+        expected: { interpolation: "a\\`b\\`c", noInterpolation: "a`b`c" },
+      },
+    ],
+    "tildes ('~')": [
+      {
+        input: "~a",
+        expected: { interpolation: "\\~a", noInterpolation: "~a" },
+      },
+      {
+        input: "~a~b",
+        expected: { interpolation: "\\~a~b", noInterpolation: "~a~b" },
+      },
+      {
+        input: "a~b",
+        expected: { interpolation: "a~b", noInterpolation: "a~b" },
+      },
+      {
+        input: "a~b~c",
+        expected: { interpolation: "a~b~c", noInterpolation: "a~b~c" },
+      },
+    ],
+    "hashtags ('#')": [
+      {
+        input: "#a",
+        expected: { interpolation: "\\#a", noInterpolation: "#a" },
+      },
+      {
+        input: "#a#b",
+        expected: { interpolation: "\\#a#b", noInterpolation: "#a#b" },
+      },
+      {
+        input: "a#b",
+        expected: { interpolation: "a#b", noInterpolation: "a#b" },
+      },
+      {
+        input: "a#b#c",
+        expected: { interpolation: "a#b#c", noInterpolation: "a#b#c" },
+      },
+    ],
+    "dollar signs ('$')": [
+      {
+        input: "a$b",
+        expected: { interpolation: "a\\$b", noInterpolation: "a$b" },
+      },
+      {
+        input: "a$b$c",
+        expected: { interpolation: "a\\$b\\$c", noInterpolation: "a$b$c" },
+      },
+    ],
+    "ampersands ('&')": [
+      {
+        input: "a&b",
+        expected: { interpolation: "a\\&b", noInterpolation: "a&b" },
+      },
+      {
+        input: "a&b&c",
+        expected: { interpolation: "a\\&b\\&c", noInterpolation: "a&b&c" },
+      },
+    ],
+    "asterisks ('*')": [
+      {
+        input: "a*b",
+        expected: { interpolation: "a\\*b", noInterpolation: "a*b" },
+      },
+      {
+        input: "a*b*c",
+        expected: { interpolation: "a\\*b\\*c", noInterpolation: "a*b*c" },
+      },
+    ],
+    "equals ('=')": [
+      {
+        input: "=a",
+        expected: { interpolation: "\\=a", noInterpolation: "=a" },
+      },
+      {
+        input: "=a=b",
+        expected: { interpolation: "\\=a=b", noInterpolation: "=a=b" },
+      },
+      {
+        input: "a=b",
+        expected: { interpolation: "a=b", noInterpolation: "a=b" },
+      },
+      {
+        input: "a=b=c",
+        expected: { interpolation: "a=b=c", noInterpolation: "a=b=c" },
+      },
+    ],
+    "backslashes ('\\')": [
+      {
+        input: "a\\b",
+        expected: { interpolation: "a\\\\b", noInterpolation: "a\\b" },
+      },
+      {
+        input: "a\\b\\c",
+        expected: { interpolation: "a\\\\b\\\\c", noInterpolation: "a\\b\\c" },
+      },
+    ],
+    "pipes ('|')": [
+      {
+        input: "a|b",
+        expected: { interpolation: "a\\|b", noInterpolation: "a|b" },
+      },
+      {
+        input: "a|b|c",
+        expected: { interpolation: "a\\|b\\|c", noInterpolation: "a|b|c" },
+      },
+    ],
+    "semicolons (';')": [
+      {
+        input: "a;b",
+        expected: { interpolation: "a\\;b", noInterpolation: "a;b" },
+      },
+      {
+        input: "a;b;c",
+        expected: { interpolation: "a\\;b\\;c", noInterpolation: "a;b;c" },
+      },
+    ],
+    "question marks ('?')": [
+      {
+        input: "a?b",
+        expected: { interpolation: "a\\?b", noInterpolation: "a?b" },
+      },
+      {
+        input: "a?b?c",
+        expected: { interpolation: "a\\?b\\?c", noInterpolation: "a?b?c" },
+      },
+    ],
+    "parentheses ('(', ')')": [
+      {
+        input: "a(b",
+        expected: { interpolation: "a\\(b", noInterpolation: "a(b" },
+      },
+      {
+        input: "a)b",
+        expected: { interpolation: "a\\)b", noInterpolation: "a)b" },
+      },
+      {
+        input: "a(b(c",
+        expected: { interpolation: "a\\(b\\(c", noInterpolation: "a(b(c" },
+      },
+      {
+        input: "a)b)c",
+        expected: { interpolation: "a\\)b\\)c", noInterpolation: "a)b)c" },
+      },
+      {
+        input: "a(b)c",
+        expected: { interpolation: "a\\(b\\)c", noInterpolation: "a(b)c" },
+      },
+      {
+        input: "a(b,c)d",
+        expected: { interpolation: "a\\(b,c\\)d", noInterpolation: "a(b,c)d" },
+      },
+    ],
+    "square brackets ('[', ']')": [
+      {
+        input: "a[b",
+        expected: { interpolation: "a\\[b", noInterpolation: "a[b" },
+      },
+      {
+        input: "a]b",
+        expected: { interpolation: "a\\]b", noInterpolation: "a]b" },
+      },
+      {
+        input: "a[b[c",
+        expected: { interpolation: "a\\[b\\[c", noInterpolation: "a[b[c" },
+      },
+      {
+        input: "a]b]c",
+        expected: { interpolation: "a\\]b\\]c", noInterpolation: "a]b]c" },
+      },
+      {
+        input: "a[b]c",
+        expected: { interpolation: "a\\[b\\]c", noInterpolation: "a[b]c" },
+      },
+      {
+        input: "a[b,c]d",
+        expected: { interpolation: "a\\[b,c\\]d", noInterpolation: "a[b,c]d" },
+      },
+    ],
+    "curly brackets ('{', '}')": [
+      {
+        input: "a{b",
+        expected: { interpolation: "a\\{b", noInterpolation: "a{b" },
+      },
+      {
+        input: "a}b",
+        expected: { interpolation: "a\\}b", noInterpolation: "a}b" },
+      },
+      {
+        input: "a{b{c",
+        expected: { interpolation: "a\\{b\\{c", noInterpolation: "a{b{c" },
+      },
+      {
+        input: "a}b}c",
+        expected: { interpolation: "a\\}b\\}c", noInterpolation: "a}b}c" },
+      },
+      {
+        input: "a{b}c",
+        expected: { interpolation: "a\\{b\\}c", noInterpolation: "a{b}c" },
+      },
+      {
+        input: "a{b,c}d",
+        expected: { interpolation: "a\\{b,c\\}d", noInterpolation: "a{b,c}d" },
+      },
+      {
+        input: "a{b,{c,d},e}f",
+        expected: {
+          interpolation: "a\\{b,\\{c,d\\},e\\}f",
+          noInterpolation: "a{b,{c,d},e}f",
+        },
+      },
+      {
+        input: "a{0..2}b",
+        expected: {
+          interpolation: "a\\{0..2\\}b",
+          noInterpolation: "a{0..2}b",
+        },
       },
     ],
     "angle brackets ('<', '>')": [
