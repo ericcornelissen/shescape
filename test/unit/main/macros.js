21a7d272d40b98039a5d582f258ffa574e243b4d
Eric Cornelissen <ericornelissen@gmail.com>
2022-03-23 23:27:02 +0100

    Unit test main.js with AVA

---
+++ test/unit/main/macros.js
@@ -0,0 +1,58 @@
+/**
+ * @overview Contains AVA test macros for unit tests.
+ * @license Unlicense
+ * @author Eric Cornelissen <ericornelissen@gmail.com>
+ */
+
+import test from "ava";
+import sinon from "sinon";
+
+/**
+ * The escapeSuccess macro tests how a specific argument is escaped.
+ *
+ * @param {Object} args The arguments for this macro.
+ * @param {Object} args.expected The value that is expected to be escaped.
+ * @param {string} args.fn The function to do the escaping.
+ * @param {Object} args.process The input argument.
+ */
+export const escapeSuccess = test.macro({
+  exec(t, { expected, fn, input }) {
+    t.context.args.arg = input;
+
+    fn(t.context.args, t.context.deps);
+    t.true(
+      t.context.deps.escapeFunction.calledWithExactly(expected, sinon.match.any)
+    );
+  },
+  title(_, { input }) {
+    return `escaping ${input}`;
+  },
+});
+
+/**
+ * The escapeTypeError macro tests that a specific argument results in a
+ * {@link TypeError}.
+ *
+ * @param {Object} args The arguments for this macro.
+ * @param {Object} args.expected The value that is expected to be escaped.
+ * @param {string} args.fn The function to do the escaping.
+ * @param {Object} args.process The input argument.
+ */
+export const escapeTypeError = test.macro({
+  exec(t, { fn, input }) {
+    t.context.args.arg = input;
+
+    t.throws(() => fn(t.context.args, t.context.deps), {
+      instanceOf: TypeError,
+      message:
+        "Shescape requires strings or values that can be converted into a string using .toString()",
+    });
+  },
+  title(providedTitle, { input }) {
+    if (providedTitle) {
+      return `escaping when ${providedTitle}`;
+    } else {
+      return `escaping ${input}`;
+    }
+  },
+});
