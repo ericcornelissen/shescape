49e5871278c38fd20f9aa1bbfadb868d6a6d81df
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-23 00:43:36 +0200

    Refactor e2e child_process test suite's macro
    - Order macros alphabetically
    - Fix the generated title for `execFile`, include the `callback`
      argument.
    - Fix JSDoc for macro arguments. There's no object capturing all input
      arguments.

--- test/e2e/child_process/_macros.js
+++ test/e2e/child_process/_macros.js
@@ -11,11 +11,130 @@ import test from "ava";
 import * as shescape from "../../../index.js";
 
 /**
+ * The execFile macro tests Shescape usage with {@link cp.execFile} for the
+ * provided `options`.
+ *
+ * @param {Object} execFileOptions The `options` for {@link cp.execFile}.
+ */
+export const execFile = test.macro({
+  exec(t, execFileOptions) {
+    t.plan(1);
+
+    const benignInput = "foobar";
+    const maliciousInput = "&& ls";
+    const args = [benignInput, maliciousInput];
+
+    const safeArgs = execFileOptions?.shell
+      ? shescape.quoteAll(args, execFileOptions)
+      : shescape.escapeAll(args, execFileOptions);
+
+    return new Promise((resolve) => {
+      cp.execFile("echo", safeArgs, execFileOptions, (error, stdout) => {
+        if (error) {
+          t.fail(`an unexpected error occurred: ${error}`);
+        } else {
+          const actual = `${stdout}`;
+          const expected = `${benignInput} ${maliciousInput}\n`;
+          t.is(actual, expected);
+        }
+
+        resolve();
+      });
+    });
+  },
+  title(_, execFileOptions) {
+    const options = execFileOptions
+      ? `, ${JSON.stringify(execFileOptions)}`
+      : "";
+    return `execFile(command, args${options}, callback)`;
+  },
+});
+
+/**
+ * The execFileSync macro tests Shescape usage with {@link cp.execFileSync} for
+ * the provided `options`.
+ *
+ * @param {Object} execFileOptions The `options` for {@link cp.execFileSync}.
+ */
+export const execFileSync = test.macro({
+  exec(t, execFileOptions) {
+    const benignInput = "foobar";
+    const maliciousInput = "&& ls";
+    const args = [benignInput, maliciousInput];
+
+    const safeArgs = execFileOptions?.shell
+      ? shescape.quoteAll(args, execFileOptions)
+      : shescape.escapeAll(args, execFileOptions);
+
+    try {
+      const stdout = cp.execFileSync("echo", safeArgs, execFileOptions);
+      const actual = `${stdout}`;
+      const expected = `${benignInput} ${maliciousInput}\n`;
+      t.is(actual, expected);
+    } catch (error) {
+      t.fail(`an unexpected error occurred: ${error}`);
+    }
+  },
+  title(_, execFileOptions) {
+    const options = execFileOptions
+      ? `, ${JSON.stringify(execFileOptions)}`
+      : "";
+    return `execFileSync(command, args${options})`;
+  },
+});
+
+/**
+ * The fork macro tests Shescape usage with {@link cp.fork} for the provided
+ * `options`.
+ *
+ * NOTE: `options.silent` is always set to `true`.
+ *
+ * @param {Object} baseForkOptions The (base) `options` for {@link cp.fork}.
+ */
+export const fork = test.macro({
+  exec(t, baseForkOptions) {
+    t.plan(1);
+
+    const forkOptions = {
+      ...baseForkOptions,
+      silent: true, // Must be set to ensure stdout is available in the test
+    };
+
+    const benignInput = "foobar";
+    const maliciousInput = "&& ls";
+    const args = [benignInput, maliciousInput];
+
+    const safeArgs = shescape.escapeAll(args, forkOptions);
+
+    return new Promise((resolve) => {
+      const echo = cp.fork("test/fuzz/echo.js", safeArgs, forkOptions);
+
+      echo.on("close", resolve);
+
+      echo.stdout.on("data", (data) => {
+        const actual = `${data}`;
+        const expected = `${benignInput} ${maliciousInput}`;
+        t.is(actual, expected);
+      });
+
+      echo.on("error", (error) => {
+        t.fail(`an unexpected error occurred: ${error}`);
+      });
+    });
+  },
+  title(_, baseForkOptions) {
+    const options = baseForkOptions
+      ? `, ${JSON.stringify(baseForkOptions)}`
+      : "";
+    return `fork(modulePath, args${options})`;
+  },
+});
+
+/**
  * The spawn macro tests Shescape usage with {@link cp.spawn} for the provided
  * `options`.
  *
- * @param {Object} args The arguments for this macro.
- * @param {Object} args.spawnOptions The `options` for {@link cp.spawn}.
+ * @param {Object} spawnOptions The `options` for {@link cp.spawn}.
  */
 export const spawn = test.macro({
   exec(t, spawnOptions) {
@@ -55,8 +174,7 @@ export const spawn = test.macro({
  * The spawn macro tests Shescape usage with {@link cp.spawnSync} for the
  * provided `options`.
  *
- * @param {Object} args The arguments for this macro.
- * @param {Object} args.spawnOptions The `options` for {@link cp.spawnSync}.
+ * @param {Object} spawnOptions The `options` for {@link cp.spawnSync}.
  */
 export const spawnSync = test.macro({
   exec(t, spawnOptions) {
@@ -80,126 +198,5 @@ export const spawnSync = test.macro({
   title(_, spawnOptions) {
     const options = spawnOptions ? `, ${JSON.stringify(spawnOptions)}` : "";
     return `spawnSync(command, args${options})`;
-  },
-});
-
-/**
- * The execFile macro tests Shescape usage with {@link cp.execFile} for the
- * provided `options`.
- *
- * @param {Object} args The arguments for this macro.
- * @param {Object} args.execFileOptions The `options` for {@link cp.execFile}.
- */
-export const execFile = test.macro({
-  exec(t, execFileOptions) {
-    t.plan(1);
-
-    const benignInput = "foobar";
-    const maliciousInput = "&& ls";
-    const args = [benignInput, maliciousInput];
-
-    const safeArgs = execFileOptions?.shell
-      ? shescape.quoteAll(args, execFileOptions)
-      : shescape.escapeAll(args, execFileOptions);
-
-    return new Promise((resolve) => {
-      cp.execFile("echo", safeArgs, execFileOptions, (error, stdout) => {
-        if (error) {
-          t.fail(`an unexpected error occurred: ${error}`);
-        } else {
-          const actual = `${stdout}`;
-          const expected = `${benignInput} ${maliciousInput}\n`;
-          t.is(actual, expected);
-        }
-
-        resolve();
-      });
-    });
-  },
-  title(_, execFileOptions) {
-    const options = execFileOptions
-      ? `, ${JSON.stringify(execFileOptions)}`
-      : "";
-    return `execFile(command, args${options})`;
-  },
-});
-
-/**
- * The execFileSync macro tests Shescape usage with {@link cp.execFileSync} for
- * the provided `options`.
- *
- * @param {Object} args The arguments for this macro.
- * @param {Object} args.execFileOptions The `options` for {@link cp.execFileSync}.
- */
-export const execFileSync = test.macro({
-  exec(t, execFileOptions) {
-    const benignInput = "foobar";
-    const maliciousInput = "&& ls";
-    const args = [benignInput, maliciousInput];
-
-    const safeArgs = execFileOptions?.shell
-      ? shescape.quoteAll(args, execFileOptions)
-      : shescape.escapeAll(args, execFileOptions);
-
-    try {
-      const stdout = cp.execFileSync("echo", safeArgs, execFileOptions);
-      const actual = `${stdout}`;
-      const expected = `${benignInput} ${maliciousInput}\n`;
-      t.is(actual, expected);
-    } catch (error) {
-      t.fail(`an unexpected error occurred: ${error}`);
-    }
-  },
-  title(_, execFileOptions) {
-    const options = execFileOptions
-      ? `, ${JSON.stringify(execFileOptions)}`
-      : "";
-    return `execFileSync(command, args${options})`;
-  },
-});
-
-/**
- * The fork macro tests Shescape usage with {@link cp.fork} for the provided
- * `options`.
- *
- * @param {Object} args The arguments for this macro.
- * @param {Object} args.baseForkOptions The base `options` for {@link cp.fork}.
- */
-export const fork = test.macro({
-  exec(t, baseForkOptions) {
-    t.plan(1);
-
-    const forkOptions = {
-      ...baseForkOptions,
-      silent: true, // Must be set to ensure stdout is available in the test
-    };
-
-    const benignInput = "foobar";
-    const maliciousInput = "&& ls";
-    const args = [benignInput, maliciousInput];
-
-    const safeArgs = shescape.escapeAll(args, forkOptions);
-
-    return new Promise((resolve) => {
-      const echo = cp.fork("test/fuzz/echo.js", safeArgs, forkOptions);
-
-      echo.on("close", resolve);
-
-      echo.stdout.on("data", (data) => {
-        const actual = `${data}`;
-        const expected = `${benignInput} ${maliciousInput}`;
-        t.is(actual, expected);
-      });
-
-      echo.on("error", (error) => {
-        t.fail(`an unexpected error occurred: ${error}`);
-      });
-    });
-  },
-  title(_, baseForkOptions) {
-    const options = baseForkOptions
-      ? `, ${JSON.stringify(baseForkOptions)}`
-      : "";
-    return `fork(modulePath, args${options})`;
   },
 });
