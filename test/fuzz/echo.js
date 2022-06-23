9f411f54cb68d0f9bb2c36f8115dfc5c65057c0d
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-23 22:25:03 +0200

    Fix echo script for zero arguments

--- test/fuzz/echo.js
+++ test/fuzz/echo.js
@@ -6,22 +6,20 @@
 
 import process from "node:process";
 
-const stdout = process.argv
-  // Slice of the "node" and "path/to/this-file.js" arguments as those shouldn't
-  // be echoed.
-  .slice(2)
-  // Protect against `argToEcho` being undefined, which causes an error when
-  // writing to `process.stdout`. `argToEcho` will be undefined when using
-  // certain shells if you provide an empty string as argument.
-  .map((arg) => arg || "")
-  // Reduce arguments to a single string to print all at once. This prevents
-  // potential problems due to a partial completed output buffer being flushed.
-  .reduce((previousValue, currentValue, currentIndex) => {
-    if (currentIndex === 0) {
-      return currentValue;
-    } else {
-      return `${previousValue} ${currentValue}`;
-    }
-  });
+if (process.argv.length > 2) {
+  const stdout = process.argv
+    // Slice of the "node" and "path/to/this-file.js" arguments as those
+    // shouldn't be echoed.
+    .slice(2)
+    // Reduce arguments to a single string to print all at once. This prevents
+    // potential problems due to a partial completed output buffer being flushed.
+    .reduce((previousValue, currentValue, currentIndex) => {
+      if (currentIndex === 0) {
+        return currentValue;
+      } else {
+        return `${previousValue} ${currentValue}`;
+      }
+    });
 
-process.stdout.write(stdout);
+  process.stdout.write(stdout);
+}
