446a68694a0b4586aab1cffce03bab0ac15ab345
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-24 21:28:49 +0200

    Re-add protection against potentially `undefined` arguments

--- test/fuzz/echo.js
+++ test/fuzz/echo.js
@@ -10,11 +10,13 @@ const stdout = process.argv
   // Slice off the "node" and "path/to/this/file.js" arguments as those
   // shouldn't be echoed.
   .slice(2)
+  // Protect against arguments being `undefined`. On certain shells, if an
+  // argument is an empty string it's `undefined`.
+  ?.map((arg) => arg || "")
   // Reduce arguments to a single string to print all at once. This prevents
   // unexpected behaviour due to a partial output buffer being flushed.
   ?.reduce(
-    (previousValue, currentValue, currentIndex) =>
-      currentIndex === 0 ? currentValue : `${previousValue} ${currentValue}`,
+    (acc, arg, argIndex) => (argIndex === 0 ? arg : `${acc} ${arg}`),
     ""
   );
 
