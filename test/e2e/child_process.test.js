a243927447102f70f6d837a8d27d4854c27f917e
Eric Cornelissen <ericornelissen@gmail.com>
2022-06-30 23:23:22 +0200

    Add child_process e2e tests for double quotes
    Expand the e2e test suite for testing with the child_process package to
    include test cases with double qoutes. This is relevant to Windows
    systems, where double quotes may be excessively escaped.

--- test/e2e/child_process.test.js
+++ test/e2e/child_process.test.js
@@ -10,33 +10,46 @@ import { macros } from "./_.js";
 
 test(macros.exec, { arg: "&& ls" });
 test(macros.exec, { arg: "' ls" });
+test(macros.exec, { arg: '" ls' });
 test(macros.exec, { arg: "&& ls", options: { shell: true } });
 test(macros.exec, { arg: "' ls", options: { shell: true } });
+test(macros.exec, { arg: '" ls', options: { shell: true } });
 
 test(macros.execSync, { arg: "&& ls" });
 test(macros.execSync, { arg: "' ls" });
+test(macros.execSync, { arg: '" ls' });
 test(macros.execSync, { arg: "&& ls", options: { shell: true } });
 test(macros.execSync, { arg: "' ls", options: { shell: true } });
+test(macros.execSync, { arg: '" ls', options: { shell: true } });
 
 test(macros.execFile, { arg: "&& ls" });
 test(macros.execFile, { arg: "' ls" });
+test(macros.execFile, { arg: '" ls' });
 test(macros.execFile, { arg: "&& ls", options: { shell: true } });
 test(macros.execFile, { arg: "' ls", options: { shell: true } });
+test(macros.execFile, { arg: '" ls', options: { shell: true } });
 
 test(macros.execFileSync, { arg: "&& ls" });
 test(macros.execFileSync, { arg: "' ls" });
+test(macros.execFileSync, { arg: '" ls' });
 test.skip(macros.execFileSync, { arg: "&& ls", options: { shell: true } }); // Skipped due to https://github.com/nodejs/node/issues/43333
 test.skip(macros.execFileSync, { arg: "' ls", options: { shell: true } }); // Skipped due to https://github.com/nodejs/node/issues/43333
+test.skip(macros.execFileSync, { arg: '" ls', options: { shell: true } }); // Skipped due to https://github.com/nodejs/node/issues/43333
 
 test(macros.fork, { arg: "&& ls" });
 test(macros.fork, { arg: "' ls" });
+test(macros.fork, { arg: '" ls' });
 
 test(macros.spawn, { arg: "&& ls" });
 test(macros.spawn, { arg: "' ls" });
+test(macros.spawn, { arg: '" ls' });
 test(macros.spawn, { arg: "&& ls", options: { shell: true } });
 test(macros.spawn, { arg: "' ls", options: { shell: true } });
+test(macros.spawn, { arg: '" ls', options: { shell: true } });
 
 test(macros.spawnSync, { arg: "&& ls" });
 test(macros.spawnSync, { arg: "' ls" });
+test(macros.spawnSync, { arg: '" ls' });
 test(macros.spawnSync, { arg: "&& ls", options: { shell: true } });
 test(macros.spawnSync, { arg: "' ls", options: { shell: true } });
+test(macros.spawnSync, { arg: '" ls', options: { shell: true } });
