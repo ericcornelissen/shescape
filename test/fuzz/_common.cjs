325ef861240b9620a795207df9c1b674dbf62f7c
Eric Cornelissen <ericornelissen@gmail.com>
2022-07-06 00:05:13 +0200

    Simplify `prepareArg`
    Omit the useless assignment to `result` and put space between the if
    statements for readability.

--- test/fuzz/_common.cjs
+++ test/fuzz/_common.cjs
@@ -52,12 +52,12 @@ function getFuzzShell() {
 }
 
 function prepareArg({ arg, quoted, shell }, disableExtraWindowsPreparations) {
-  let result = arg;
   if (isShellCmd(shell)) {
     // In CMD ignores everything after a newline (\n) character. This alteration
     // is required even when `disableExtraWindowsPreparations` is true.
-    result = result.replace(/[\n\r]+/g, "");
+    arg = arg.replace(/[\n\r]+/g, "");
   }
+
   if (isWindows() && !disableExtraWindowsPreparations) {
     // Node on Windows ...
     if (isShellCmd(shell)) {
@@ -65,39 +65,39 @@ function prepareArg({ arg, quoted, shell }, disableExtraWindowsPreparations) {
       if (quoted) {
         // ... interprets arguments with `\"` as `"` (even if there's a
         // null character between `\` and `"`) so we escape the `\`.
-        result = result.replace(/((\\\u{0}*)+)(?=\u{0}*("|$))/gu, "$1$1");
+        arg = arg.replace(/((\\\u{0}*)+)(?=\u{0}*("|$))/gu, "$1$1");
       } else {
         // ... interprets arguments with `\"` as `"` so we escape the `\` ...
-        result = result.replace(/((\\\u{0}*)+)(?=\u{0}*")/gu, "$1$1");
+        arg = arg.replace(/((\\\u{0}*)+)(?=\u{0}*")/gu, "$1$1");
 
         // ... interprets arguments with `"` as `` so we escape it with `\`.
-        result = result.replace(/"/g, `\\"`);
+        arg = arg.replace(/"/g, `\\"`);
       }
     } else if (isShellPowerShell(shell)) {
       // ... in PowerShell, depending on if there's whitespace in the
       // argument ...
-      if (/\s|\u0085/g.test(result) && quoted) {
+      if (/\s|\u0085/g.test(arg) && quoted) {
         // ... interprets arguments with `""` as nothing so we escape it with
         // extra double quotes as `""""` ...
-        result = result.replace(/"/g, `""`);
+        arg = arg.replace(/"/g, `""`);
 
         // ... and interprets arguments with `\"` as `"` (even if there's a null
         // character between `\` and `"`) so we escape the `\`.
-        result = result.replace(/((\\\u{0}*)+)(?=\u{0}*("|$))/gu, "$1$1");
+        arg = arg.replace(/((\\\u{0}*)+)(?=\u{0}*("|$))/gu, "$1$1");
       } else {
         // ... interprets arguments with `\"` as `"` (even if there's a null
         // character between `\` and `"`) so we escape the `\`, except that the
         // quote closing the argument cannot be escaped ...
-        result = result.replace(/((\\\u{0}*)+)(?=\u{0}*("))/gu, "$1$1");
+        arg = arg.replace(/((\\\u{0}*)+)(?=\u{0}*("))/gu, "$1$1");
 
         // ... and interprets arguments with `""` as nothing so we escape it
         // with `\"`.
-        result = result.replace(/"/g, `\\"`);
+        arg = arg.replace(/"/g, `\\"`);
       }
     }
   }
 
-  return result;
+  return arg;
 }
 
 module.exports = {
