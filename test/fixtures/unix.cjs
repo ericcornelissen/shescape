a5740f7545e4dee7a16b0aa828f924d897024bff
Eric Cornelissen <ericornelissen@gmail.com>
2022-07-06 00:22:08 +0200

    Update test fixtures for Unix shells
    Update the test fixtures (used for unit and integration tests) for
    all shells on Unix to include exmaple where characters need to be
    escaped because they appear after whitespace (thus far represented only
    by a space character, ` `). Namely:
    
    - `~` (for Bash, Dash, and Zsh)
    - `#` (for Bash, Dash, and Zsh)
    - `=` (for Zsh)

--- test/fixtures/unix.cjs
+++ test/fixtures/unix.cjs
@@ -165,6 +165,10 @@ module.exports.escape = {
         input: "a=~ ",
         expected: { interpolation: "a=\\~ ", noInterpolation: "a=~ " },
       },
+      {
+        input: "a ~b",
+        expected: { interpolation: "a \\~b", noInterpolation: "a ~b" },
+      },
     ],
     "hashtags ('#')": [
       {
@@ -182,6 +186,10 @@ module.exports.escape = {
       {
         input: "a#b#c",
         expected: { interpolation: "a#b#c", noInterpolation: "a#b#c" },
+      },
+      {
+        input: "a #b",
+        expected: { interpolation: "a \\#b", noInterpolation: "a #b" },
       },
     ],
     "dollar signs ('$')": [
@@ -558,6 +566,10 @@ module.exports.escape = {
         input: "a=~ ",
         expected: { interpolation: "a=~ ", noInterpolation: "a=~ " },
       },
+      {
+        input: "a ~b",
+        expected: { interpolation: "a \\~b", noInterpolation: "a ~b" },
+      },
     ],
     "hashtags ('#')": [
       {
@@ -575,6 +587,10 @@ module.exports.escape = {
       {
         input: "a#b#c",
         expected: { interpolation: "a#b#c", noInterpolation: "a#b#c" },
+      },
+      {
+        input: "a #b",
+        expected: { interpolation: "a \\#b", noInterpolation: "a #b" },
       },
     ],
     "dollar signs ('$')": [
@@ -871,6 +887,10 @@ module.exports.escape = {
         input: "a~b~c",
         expected: { interpolation: "a~b~c", noInterpolation: "a~b~c" },
       },
+      {
+        input: "a ~b",
+        expected: { interpolation: "a \\~b", noInterpolation: "a ~b" },
+      },
     ],
     "hashtags ('#')": [
       {
@@ -888,6 +908,10 @@ module.exports.escape = {
       {
         input: "a#b#c",
         expected: { interpolation: "a#b#c", noInterpolation: "a#b#c" },
+      },
+      {
+        input: "a #b",
+        expected: { interpolation: "a \\#b", noInterpolation: "a #b" },
       },
     ],
     "dollar signs ('$')": [
@@ -936,6 +960,10 @@ module.exports.escape = {
       {
         input: "a=b=c",
         expected: { interpolation: "a=b=c", noInterpolation: "a=b=c" },
+      },
+      {
+        input: "a =b",
+        expected: { interpolation: "a \\=b", noInterpolation: "a =b" },
       },
     ],
     "backslashes ('\\')": [
