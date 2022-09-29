615b50ae9b31ae1da5897f4273900328afee64a9
Eric Cornelissen <ericornelissen@gmail.com>
2022-09-29 22:56:15 +0200

    Fix validate-codecov-config.sh

--- script/validate-codecov-config.sh
+++ script/validate-codecov-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 RESULT=$(curl -s -o /dev/null -w "%{http_code}" --data-binary @./.github/codecov.yml https://codecov.io/validate)
-if [ $RESULT != "200" ]; then
+if [ "$RESULT" != "200" ]; then
   echo "Codecov configuration is invalid"
   exit 1
 else
