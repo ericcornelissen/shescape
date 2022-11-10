ad70177cc7f189cb5a3dade8aacd0ecc36a277c7
Eric Cornelissen <ericornelissen@gmail.com>
2022-11-10 10:05:05 +0100

    Merge index::escapeAll property tests into integration tests
    Take the property tests for the `shescape.escapeAll` function and merge
    them into the integration tests for the `shescape.escapeAll` function.

--- test/integration/escape-all.test.js
+++ test/integration/escape-all.test.js
@@ -3,9 +3,11 @@
  * @license Unlicense
  */
 
+import { testProp } from "@fast-check/ava";
 import test from "ava";
+import * as fc from "fast-check";
 
-import { macros } from "./_.js";
+import { arbitrary, macros } from "./_.js";
 
 import { escapeAll as escapeAllEsm } from "../../index.js";
 import { escapeAll as escapeAllCjs } from "../../index.cjs";
@@ -19,6 +21,40 @@ for (const { escapeAll, type } of cases) {
   test(type, macros.escapeAllSuccess, { escapeAll });
   test(type, macros.escapeAllNonArray, { escapeAll });
   test(type, macros.escapeAllFailure, { escapeAll });
+
+  testProp(
+    `return values (${type})`,
+    [fc.array(fc.string()), arbitrary.shescapeOptions()],
+    (t, args, options) => {
+      const result = escapeAll(args, options);
+      for (const entry of result) {
+        t.is(typeof entry, "string");
+      }
+
+      t.pass(); // in case `result.length === 0`
+    }
+  );
+
+  testProp(
+    `return size (${type})`,
+    [fc.array(fc.string()), arbitrary.shescapeOptions()],
+    (t, args, options) => {
+      const result = escapeAll(args, options);
+      t.is(result.length, args.length);
+    }
+  );
+
+  testProp(
+    `string as input (${type})`,
+    [fc.string(), arbitrary.shescapeOptions()],
+    (t, arg, options) => {
+      const result = escapeAll(arg, options);
+      t.is(result.length, 1);
+
+      const entry = result[0];
+      t.is(typeof entry, "string");
+    }
+  );
 
   test(type, macros.prototypePollution, (_, payload) => {
     escapeAll(["a"], payload);
