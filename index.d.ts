6c7ee301d203503febbdb0e9e594699282db83db
Eric Cornelissen <ericornelissen@gmail.com>
2022-12-23 22:00:35 +0100

    Add JSDoc to `index.d.ts` (#568)
    * Copy JSDoc from `index.js` to `index.d.ts` verbatim
    
    Copying the JSDoc over from `index.js` to `index.d.ts` ensures all IDEs
    properly get the JSDoc and display it to users (regardless of which file
    it's pulled from).
    
    Verbatim copy includes some redundant information for TypeScript (as
    it's encoded by the types), but makes it simpler to maintain as there's
    no manual changes required when copy the documentation from `index.js`
    to `index.d.ts`.
    
    * Add JSDoc to `.d.ts` for not directly-exported types
    
    * Update CHANGELOG.md

--- index.d.ts
+++ index.d.ts
@@ -3,21 +3,103 @@
  * @license MPL-2.0
  */
 
+/**
+ * Possible values of a shell. `false` and `undefined` mean no shell. `true`
+ * means the default system shell, and any non-empty string configures a
+ * particular shell.
+ */
 type ShellOption = boolean | string | undefined;
 
+/**
+ * Options for {@link escape} and {@link escapeAll}.
+ */
 interface EscapeOptions {
+  /**
+   * Is interpolation enabled.
+   *
+   * @default false
+   */
   readonly interpolation?: boolean;
+
+  /**
+   * The shell to escape for.
+   *
+   * @default undefined
+   */
   readonly shell?: ShellOption;
 }
 
+/**
+ * Options for {@link quote} and {@link quoteAll}.
+ */
 interface QuoteOptions {
+  /**
+   * The shell to escape for.
+   *
+   * @default undefined
+   */
   readonly shell?: ShellOption;
 }
 
+/**
+ * Take a single value, the argument, and escape any dangerous characters.
+ *
+ * Non-string inputs will be converted to strings using a `toString()` method.
+ *
+ * @param {string} arg The argument to escape.
+ * @param {object} [options] The escape options.
+ * @param {boolean} [options.interpolation=false] Is interpolation enabled.
+ * @param {boolean | string} [options.shell] The shell to escape for.
+ * @returns {string} The escaped argument.
+ * @throws {TypeError} The argument is not stringable.
+ * @since 0.1.0
+ */
 export function escape(arg: string, options?: EscapeOptions): string;
 
+/**
+ * Take a array of values, the arguments, and escape any dangerous characters in
+ * every argument.
+ *
+ * Non-array inputs will be converted to one-value arrays and non-string values
+ * will be converted to strings using a `toString()` method.
+ *
+ * @param {string[]} args The arguments to escape.
+ * @param {object} [options] The escape options.
+ * @param {boolean} [options.interpolation=false] Is interpolation enabled.
+ * @param {boolean | string} [options.shell] The shell to escape for.
+ * @returns {string[]} The escaped arguments.
+ * @throws {TypeError} One of the arguments is not stringable.
+ * @since 1.1.0
+ */
 export function escapeAll(args: string[], options?: EscapeOptions): string[];
 
+/**
+ * Take a single value, the argument, put OS-specific quotes around it and
+ * escape any dangerous characters.
+ *
+ * Non-string inputs will be converted to strings using a `toString()` method.
+ *
+ * @param {string} arg The argument to quote and escape.
+ * @param {object} [options] The escape and quote options.
+ * @param {boolean | string} [options.shell] The shell to escape for.
+ * @returns {string} The quoted and escaped argument.
+ * @throws {TypeError} The argument is not stringable.
+ * @since 0.3.0
+ */
 export function quote(arg: string, options?: QuoteOptions): string;
 
+/**
+ * Take an array of values, the arguments, put OS-specific quotes around every
+ * argument and escape any dangerous characters in every argument.
+ *
+ * Non-array inputs will be converted to one-value arrays and non-string values
+ * will be converted to strings using a `toString()` method.
+ *
+ * @param {string[]} args The arguments to quote and escape.
+ * @param {object} [options] The escape and quote options.
+ * @param {boolean | string} [options.shell] The shell to escape for.
+ * @returns {string[]} The quoted and escaped arguments.
+ * @throws {TypeError} One of the arguments is not stringable.
+ * @since 0.4.0
+ */
 export function quoteAll(args: string[], options?: QuoteOptions): string[];
