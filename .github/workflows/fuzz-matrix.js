4f94e86dbf923778ca46a0a9c36c469e66220001
Eric Cornelissen <ericornelissen@gmail.com>
2022-12-28 14:07:38 +0100

    Fuzz fork only with system shell
    Defining a shell is not supported by fork, so fuzzing it with various
    shells is pointless

--- .github/workflows/fuzz-matrix.js
+++ .github/workflows/fuzz-matrix.js
@@ -12,19 +12,19 @@ const systemShell = "";
 const unixShells = [systemShell, "/bin/bash", "/bin/dash", "/bin/zsh"];
 const winShells = [systemShell, "cmd.exe", "powershell.exe"];
 
-const targets = ["exec", "exec-file", "fork", "spawn"];
+const targets = ["exec", "exec-file", "spawn"];
 
 export function determineMatrix({ unix, windows }) {
   return [
     ...(unix
       ? unixShells.flatMap((shell) =>
           targets.map((target) => ({ os: unixOS, shell, target }))
-        )
+        ).concat([{ os: unixOS, shell: systemShell, target: "fork" }])
       : []),
     ...(windows
       ? winShells.flatMap((shell) =>
           targets.map((target) => ({ os: winOS, shell, target }))
-        )
+        ).concat([{ os: winOS, shell: systemShell, target: "fork" }])
       : []),
   ];
 }
