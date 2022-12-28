0cc1acfde2a36c24f9bbb93806fc4db06d2f2bec
Eric Cornelissen <ericornelissen@gmail.com>
2022-12-28 23:46:18 +0100

    Adjust argument preperation for PowerShell

--- test/fuzz/_common.cjs
+++ test/fuzz/_common.cjs
@@ -147,8 +147,8 @@ function prepareArg({ arg, quoted, shell }, disableExtraWindowsPreparations) {
           "$1$1"
         );
       } else if (
-        /(?<!^[\t\n\v\f \u0085\u00A0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000\uFEFF]*)[\t\n\v\f \u0085\u00A0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000\uFEFF]/u.test(
-          arg
+        /(?<!^)[\t\n\v\f \u0085\u00A0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000\uFEFF]/u.test(
+          arg.trimStart()
         ) &&
         !quoted
       ) {
