3975585d79640b11b8880b2e26d0d2435adec51e
Eric Cornelissen <ericornelissen@gmail.com>
2023-01-12 22:12:31 +0100

    Further reduce duplication in `child_process.test.js` e2e test

--- test/e2e/child_process.test.js
+++ test/e2e/child_process.test.js
@@ -9,25 +9,16 @@ import test from "ava";
 import { macros } from "./_.js";
 
 const testArgs = ["&& ls", "' ls", '" ls'];
+const testOptions = [undefined, { shell: true }];
 
 for (const arg of testArgs) {
-  test(macros.exec, { arg });
-  test(macros.exec, { arg, options: { shell: true } });
-
-  test(macros.execSync, { arg });
-  test(macros.execSync, { arg, options: { shell: true } });
-
-  test(macros.execFile, { arg });
-  test(macros.execFile, { arg, options: { shell: true } });
-
-  test(macros.execFileSync, { arg });
-  test(macros.execFileSync, { arg, options: { shell: true } });
-
-  test(macros.fork, { arg });
-
-  test(macros.spawn, { arg });
-  test(macros.spawn, { arg, options: { shell: true } });
-
-  test(macros.spawnSync, { arg });
-  test(macros.spawnSync, { arg, options: { shell: true } });
+  for (const options of testOptions) {
+    test(macros.exec, { arg, options });
+    test(macros.execSync, { arg, options });
+    test(macros.execFile, { arg, options });
+    test(macros.execFileSync, { arg, options });
+    test(macros.fork, { arg, options });
+    test(macros.spawn, { arg, options });
+    test(macros.spawnSync, { arg, options });
+  }
 }
