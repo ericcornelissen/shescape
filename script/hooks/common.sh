147aa8ccaa9fcdde809f17ac76184c5e31ec1219
Eric Cornelissen <ericornelissen@gmail.com>
2023-01-13 22:21:25 +0100

    Enable (some) optional ShellCheck checks (#656)
    * Enable (some) optional ShellCheck checks
    
    Update the `.shellcheckrc` configuration to enable some "optional"
    checks that seem to have obvious benefits:
    - `avoid-nullary-conditions`: makes scripts more explicit.
    - `deprecate-which`: if it's deprecated it seems sensible to avoid using
      it.
    - `quote-safe-variables`: just always quote, don't need to question it.
    - `require-variable-braces`: same, just always use braces, don't need to
      question it.
    
    * Use multi-line style for disabled rules
    
    Makes it easier to change things (typing `disable=` isn't hard), in
    particular review changes as it'll be more obvious what got added and
    what was removed.
    
    * Refactor shell scripts based on ShellCheck violations

--- script/hooks/common.sh
+++ script/hooks/common.sh
@@ -2,17 +2,17 @@
 
 get_stash_count () {
   readonly count="$(git rev-list --walk-reflogs --count refs/stash 2> /dev/null)"
-  if [ "$count" = "" ]; then
+  if [ "${count}" = "" ]; then
     echo "0"
   else
-    echo "$count"
+    echo "${count}"
   fi
 }
 
 STASH_COUNT_BEFORE="$(get_stash_count)"
 DID_STASH () {
   readonly STASH_COUNT_AFTER="$(get_stash_count)"
-  if [ "$STASH_COUNT_BEFORE" != "$STASH_COUNT_AFTER" ]; then
+  if [ "${STASH_COUNT_BEFORE}" != "${STASH_COUNT_AFTER}" ]; then
     echo "x"  # true
   else
     echo ""  # false
