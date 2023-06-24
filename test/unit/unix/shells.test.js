8ade10406410d4c06c06ab7c86f159c17f085626
Eric Cornelissen <ericornelissen@gmail.com>
2023-06-24 14:47:20 +0200

    Align unit tests for escaping with unit tests for quoting

--- test/unit/unix/shells.test.js
+++ test/unit/unix/shells.test.js
@@ -45,6 +45,16 @@ for (const [shellName, shellExports] of Object.entries(shells)) {
     });
   });
 
+  testProp(
+    `escape function for ${shellName}`,
+    [fc.string(), fc.boolean()],
+    (t, arg, interpolation) => {
+      const escapeFn = shellExports.getEscapeFunction({ interpolation });
+      const result = escapeFn(arg);
+      t.is(typeof result, "string");
+    }
+  );
+
   quoteFixtures.forEach(({ input, expected }) => {
     test(macros.quote, {
       expected,
