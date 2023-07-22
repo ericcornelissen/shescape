fce9656e08f55c48ce5234e3d79821147f1013a5
Eric Cornelissen <ericornelissen@gmail.com>
2023-07-22 21:32:22 +0200

    Improve use of fuzz coverage data (#1058)
    * Separate fuzz coverage by target and shell, gain overall coverage data
    
    Update the fuzz script to, on completion, rename the generated coverage
    file so that it's unique to the target and shell that was fuzzed. This
    results in maintaining coverage data from previous runs. In turn, using
    nyc to generate a HTML coverage report will combine all coverage files
    into one report, giving a more holistic view of fuzz coverage.
    
    Note: with the current implementation this only really works when jsfuzz
    terminates by itself, e.g. due to the usage of `--fuzzTime` or because
    of a crash.
    
    * Send `SIGINT` to jsfuzz when `SIGINT` is received by fuzz.js
    
    Capture the kill (`SIGINT`) signal received by the fuzz.js script and
    send it to jsfuzz in order to
    1. make sure the pre-exit code (currently moving coverage around) is
       run, and
    2. have cleaner exits when the fuzz script is terminated early. I.e. it
       should now be less likely to get a crash when ending a fuzzing
       session manually.
    
    * Automatically generate the fuzz coverage report
    
    Update the fuzz script to, in addition to organizing the coverage files,
    immediately generate the coverage report. Given that this may not happen
    correctly, the `npm run fuzz:coverage` command is kept (and used for
    this purpose).
    
    The Contributing Guidelines have been updated in accordance with this
    changes.
    
    It was considered to update the `reusable-fuzz.yml` workflow to include
    the coverage report in the artifact uploaded in the event of a crash,
    however the use of `timeout Xs` prevents the `SIGINT` signal hook from
    running (so no fuzz coverage report is generated).

--- script/fuzz.js
+++ script/fuzz.js
@@ -4,15 +4,16 @@
  * @license MIT
  */
 
-import * as cp from "node:child_process";
-import * as fs from "node:fs";
-import * as os from "node:os";
-import * as process from "node:process";
+import cp from "node:child_process";
+import fs from "node:fs";
+import os from "node:os";
+import process from "node:process";
 
 import { getFuzzShell } from "../test/fuzz/_common.cjs";
 
 const corpusDir = "./.corpus";
 const fuzzTargetsDir = "./test/fuzz";
+const nycOutputDir = "./.nyc_output";
 const testCasesDir = "./test/fuzz/corpus";
 
 main(process.argv.slice(2));
@@ -23,6 +24,10 @@ function main(argv) {
   prepareCorpus();
   logShellToFuzz();
   startFuzzing(fuzzTarget, fuzzTime);
+}
+
+function fuzzTargetToFuzzFile(target) {
+  return `${fuzzTargetsDir}/${target}.test.cjs`;
 }
 
 function getFuzzTarget(argv) {
@@ -46,9 +51,10 @@ function getFuzzTarget(argv) {
     process.exit(1);
   }
 
-  const target = `${fuzzTargetsDir}/${argv[0]}.test.cjs`;
-  if (!fs.existsSync(target)) {
-    console.log(`Cannot find fuzz target "${target}"`);
+  const target = argv[0];
+  const targetFile = fuzzTargetToFuzzFile(target);
+  if (!fs.existsSync(targetFile)) {
+    console.log(`Cannot find fuzz target "${targetFile}"`);
     process.exit(2);
   }
 
@@ -90,11 +96,30 @@ function prepareCorpus() {
 }
 
 function startFuzzing(target, time) {
+  const npm = os.platform() === "win32" ? "npm.cmd" : "npm";
+  const fuzzFile = fuzzTargetToFuzzFile(target);
+
   const fuzz = cp.spawn(
-    os.platform() === "win32" ? "npm.cmd" : "npm",
-    ["exec", "jsfuzz", "--", target, corpusDir, `--fuzzTime=${time}`],
+    npm,
+    ["exec", "jsfuzz", "--", fuzzFile, corpusDir, `--fuzzTime=${time}`],
     { stdio: "inherit" },
   );
 
-  fuzz.on("close", (code) => process.exit(code));
+  fuzz.on("close", (code) => {
+    console.log("Arranging (raw) coverage files");
+    const shell = (getFuzzShell() || "default-shell").replace(/[/\\]/gu, "");
+    const defaultCoverageFile = `${nycOutputDir}/cov.json`;
+    const runCoverageFile = `${nycOutputDir}/cov-${target}-${shell}.json`;
+    fs.copyFileSync(defaultCoverageFile, runCoverageFile);
+    fs.rmSync(defaultCoverageFile);
+
+    console.log("Generating coverage report");
+    cp.spawnSync(npm, ["run", "fuzz:coverage"]);
+
+    process.exit(code);
+  });
+
+  process.on("SIGINT", () => {
+    fuzz.kill("SIGINT");
+  });
 }
