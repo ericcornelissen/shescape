72b2baf0e01ff8c61d88c9fc00b88158692c8f84
Eric Cornelissen <ericornelissen@gmail.com>
2023-08-20 16:42:08 +0200

    Update throws assertions in line with the test level (#1139)
    Improve the assertions of throwing in tests in accordance with the test
    level:
    
    - Unit tests should assert the type and error message.
    - Integration tests should assert the type (to be in-line with the
      documented type).

--- test/integration/escape.test.js
+++ test/integration/escape.test.js
@@ -31,7 +31,7 @@ testProp(
 
 testProp("invalid arguments", [arbitrary.shescapeOptions()], (t, options) => {
   for (const { value } of constants.illegalArguments) {
-    t.throws(() => escape(value, options));
+    t.throws(() => escape(value, options), { instanceOf: TypeError });
   }
 });
 
