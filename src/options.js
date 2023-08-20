8ad82124417e2c94a833ebe1bd7edd8ce8fbdc74
Eric Cornelissen <ericornelissen@gmail.com>
2023-08-20 20:53:21 +0200

    Simplify interface of the internal `parseOptions` method

--- src/options.js
+++ src/options.js
@@ -10,19 +10,18 @@ import { isString } from "./reflection.js";
  * Parses options provided to shescape.
  *
  * @param {object} args The arguments for this function.
+ * @param {object} args.env The environment variables.
  * @param {object} args.options The options for escaping.
  * @param {boolean} [args.options.flagProtection] Is flag protection enabled.
  * @param {boolean} [args.options.interpolation] Is interpolation enabled.
  * @param {boolean | string} [args.options.shell] The shell to escape for.
- * @param {object} args.process The `process` values.
- * @param {object} args.process.env The environment variables.
  * @param {object} deps The dependencies for this function.
  * @param {Function} deps.getDefaultShell Function to get the default shell.
  * @param {Function} deps.getShellName Function to get the name of a shell.
  * @returns {object} The parsed arguments.
  */
 export function parseOptions(
-  { options: { flagProtection, interpolation, shell }, process: { env } },
+  { env, options: { flagProtection, interpolation, shell } },
   { getDefaultShell, getShellName },
 ) {
   flagProtection = flagProtection ? true : false;
