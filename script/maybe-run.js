de29baae3ab02e4135f337e187d429b10f54fb90
Eric Cornelissen <ericornelissen@gmail.com>
2023-10-11 22:24:40 +0200

    Don't do PATH check in CI

--- script/maybe-run.js
+++ script/maybe-run.js
@@ -1,10 +1,12 @@
 /**
  * @overview Run a tool if it's present on the PATH, otherwise output a warning.
+ * If used in a CI context, the tool will be run without any checks.
  * @license MIT-0
  */
 
 import process from "node:process";
 
+import isCI from "is-ci";
 import which from "which";
 
 import { common } from "./_.js";
@@ -17,14 +19,16 @@ if (common.argv.length === 0) {
 const cmd = common.argv[0];
 const args = common.argv.slice(1);
 
-try {
-  which.sync(cmd);
-} catch (_) {
-  console.warn(
-    `Command '${cmd}' not found, it will not be run.`,
-    "Install it to make this warning go away.",
-  );
-  process.exit(0);
+if (!isCI) {
+  try {
+    which.sync(cmd);
+  } catch (_) {
+    console.warn(
+      `Command '${cmd}' not found, it will not be run.`,
+      "Install it to make this warning go away.",
+    );
+    process.exit(0);
+  }
 }
 
 const { status } = common.spawnSync(cmd, args);
