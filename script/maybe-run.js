a6a42ddef711c667abb0ba9017f656ab23f0ca1e
Eric Cornelissen <ericornelissen@gmail.com>
2023-10-12 16:25:31 +0200

    Create script to run a command iff it's available on the `PATH` (#1228)
    Create script to run a command iff it's available on the `PATH` and use
    it to run actionlint or ShellCheck with `lint:ci` and `lint:sh` (resp.).
    The script is written so that the command is run regardless of it being
    on the path in a CI context (leveraging the `is-ci` package already used
    in this project).
    
    As a consequence of this change, the git hooks can be simplified (which
    has the added side-effect of enabling JSON linting in the hooks, which
    was previously missing).

---
+++ script/maybe-run.js
@@ -0,0 +1,35 @@
+/**
+ * @overview Run a tool if it's present on the PATH, otherwise output a warning.
+ * If used in a CI context, the tool will be run without any checks.
+ * @license MIT-0
+ */
+
+import process from "node:process";
+
+import isCI from "is-ci";
+import which from "which";
+
+import { common } from "./_.js";
+
+if (common.argv.length === 0) {
+  console.error("Provide a command to try and execute.");
+  process.exit(1);
+}
+
+const cmd = common.argv[0];
+const args = common.argv.slice(1);
+
+if (!isCI) {
+  try {
+    which.sync(cmd);
+  } catch (_) {
+    console.warn(
+      `Command '${cmd}' not found, it will not be run.`,
+      "Install it to make this warning go away.",
+    );
+    process.exit(0);
+  }
+}
+
+const { status } = common.spawnSync(cmd, args);
+process.exit(status === null ? 1 : status);
