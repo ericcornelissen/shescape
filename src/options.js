3b5d1f14843f5e4e3ebb47c6adcb41726103de25
Eric Cornelissen <ericornelissen@gmail.com>
2023-11-05 16:08:15 +0100

    Avoid non-own property lookups

--- src/options.js
+++ src/options.js
@@ -30,8 +30,6 @@ function unsupportedError(shellName) {
  * @param {object} args The arguments for this function.
  * @param {Object<string, string>} args.env The environment variables.
  * @param {object} args.options The options for escaping.
- * @param {boolean} [args.options.flagProtection] Is flag protection enabled.
- * @param {boolean | string} [args.options.shell=true] The shell to escape for.
  * @param {object} deps The dependencies for this function.
  * @param {Function} deps.getDefaultShell Function to get the default shell.
  * @param {Function} deps.getShellName Function to get the name of a shell.
@@ -40,9 +38,14 @@ function unsupportedError(shellName) {
  * @throws {Error} The shell is not supported or could not be found.
  */
 export function parseOptions(
-  { env, options: { flagProtection, shell } },
+  { env, options },
   { getDefaultShell, getShellName, isShellSupported },
 ) {
+  let flagProtection = Object.hasOwn(options, "flagProtection")
+    ? options.flagProtection
+    : undefined;
+  let shell = Object.hasOwn(options, "shell") ? options.shell : undefined;
+
   flagProtection =
     flagProtection === undefined ? true : flagProtection ? true : false;
 
