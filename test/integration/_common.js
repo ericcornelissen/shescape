c4a81eeea51edfb9df79c3b2433cdf04313fe129
Eric Cornelissen <ericornelissen@gmail.com>
2025-06-16 20:57:41 +0200

    Refactor `getTestFn` for integration
    Simplify to reduce cognitive complexity, as reported by CodeScene.

--- test/integration/_common.js
+++ test/integration/_common.js
@@ -18,22 +18,8 @@ import * as constants from "../_constants.js";
  * @returns {Function} An AVA `test` function.
  */
 export function getTestFn(shell) {
-  if (constants.isWindows) {
-    if (!constants.shellsWindows.includes(shell)) {
-      return test.skip;
-    }
-  } else if (constants.isMacOS) {
-    if (!constants.shellsUnix.includes(shell)) {
-      return test.skip;
-    }
-
-    if (shell === constants.binBusyBox) {
-      return test.skip;
-    }
-  } else {
-    if (!constants.shellsUnix.includes(shell)) {
-      return test.skip;
-    }
+  if (skipForUnix(shell) || skipForWindows(shell)) {
+    return test.skip;
   }
 
   try {
@@ -45,4 +31,32 @@ export function getTestFn(shell) {
   } catch {
     return test.skip;
   }
+}
+
+/**
+ * Determine if an integration test may be skipped for a shell on Unix systems.
+ *
+ * @param {string} shell The name of the shell to test.
+ * @returns {boolean} `true` if it may be skipped, `false` otherwise.
+ */
+function skipForUnix(shell) {
+  if (constants.isMacOS && shell === constants.binBusyBox) {
+    return true;
+  }
+
+  return !constants.shellsUnix.includes(shell);
+}
+
+/**
+ * Determine if an integration test may be skipped for a shell on Windows.
+ *
+ * @param {string} shell The name of the shell to test.
+ * @returns {boolean} `true` if it may be skipped, `false` otherwise.
+ */
+function skipForWindows(shell) {
+  if (constants.isWindows) {
+    return false;
+  }
+
+  return !constants.shellsWindows.includes(shell);
 }
