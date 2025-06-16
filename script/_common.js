818f2f1445427696e8efab88f8be1874467d1cdb
Eric Cornelissen <ericornelissen@gmail.com>
2025-06-16 23:27:06 +0200

    Accept defeat and use `child_process.exec` instead
    Refactor the script helper to spawn npm commands to use the exec API in
    favor of spawn with `shell: true` to make it more explicit(?) that this
    is potentially insecure. The usage here is fine because it's purely for
    development and the inputs are always trusted.

--- script/_common.js
+++ script/_common.js
@@ -8,8 +8,6 @@ import os from "node:os";
 import path from "node:path";
 import process from "node:process";
 
-const npmCmd = isWindows() ? "npm.cmd" : "npm";
-
 export const argv = process.argv.slice(2);
 
 export const projectRoot = path.resolve(import.meta.dirname, "..");
@@ -18,16 +16,17 @@ export function isWindows() {
   return os.platform() === "win32";
 }
 
-export function npm(argv) {
-  return cp.spawn(npmCmd, argv, {
+export function exec(command) {
+  return cp.execSync(command, {
     cwd: projectRoot,
     stdio: "inherit",
   });
 }
 
-export function npmSync(argv) {
-  return cp.spawnSync(npmCmd, argv, {
+export function execPipe(command) {
+  return cp.execSync(command, {
     cwd: projectRoot,
     encoding: "utf8",
+    stdio: "pipe",
   });
 }
