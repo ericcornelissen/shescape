64d2db2b67646d9dff8127194d6dceaf1fd034f4
Eric Cornelissen <ericornelissen@gmail.com>
2025-12-26 15:36:05 +0100

    Avoid repeated calls to `RegExp` constructor

--- src/internal/win/no-shell.js
+++ src/internal/win/no-shell.js
@@ -14,24 +14,15 @@ import RegExp from "@ericcornelissen/lregexp";
 const unsupportedError = "Quoting is not supported when no shell is used";
 
 /**
- * Escape an argument for shell-less use.
- *
- * @param {string} arg The argument to escape.
- * @returns {string} The escaped argument.
- */
-function escapeArg(arg) {
-  const controlCharacters = new RegExp("[\0\u0008\u001B\u009B]", "g");
-  const carriageReturns = new RegExp("(?:(\r\n)|\r)", "g");
-  return arg.replace(controlCharacters, "").replace(carriageReturns, "$1");
-}
-
-/**
  * Returns a function to escape arguments for shell-less use.
  *
  * @returns {function(string): string} A function to escape arguments.
  */
 export function getEscapeFunction() {
-  return escapeArg;
+  const controlCharacters = new RegExp("[\0\u0008\u001B\u009B]", "g");
+  const carriageReturns = new RegExp("(?:(\r\n)|\r)", "g");
+  return (arg) =>
+    arg.replace(controlCharacters, "").replace(carriageReturns, "$1");
 }
 
 /**
@@ -54,22 +45,11 @@ export function getQuoteFunction() {
 }
 
 /**
- * Remove any prefix from the provided argument that might be interpreted as a
- * flag on Windows systems.
- *
- * @param {string} arg The argument to update.
- * @returns {string} The updated argument.
- */
-function stripFlagPrefix(arg) {
-  const leadingHyphensAndSlashes = new RegExp("^(?:-+|/+)");
-  return arg.replace(leadingHyphensAndSlashes, "");
-}
-
-/**
  * Returns a function to protect against flag injection for Windows systems.
  *
  * @returns {function(string): string} A function to protect against flag injection.
  */
 export function getFlagProtectionFunction() {
-  return stripFlagPrefix;
+  const leadingHyphensAndSlashes = new RegExp("^(?:-+|/+)");
+  return (arg) => arg.replace(leadingHyphensAndSlashes, "");
 }
