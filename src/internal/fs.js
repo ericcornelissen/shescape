4df884c6568212c5f9b145243e341c916d7d04d6
Eric Cornelissen <ericornelissen@gmail.com>
2026-03-01 16:10:41 +0100

    Align `deps` argument of the file system facade with other functions

--- src/internal/fs.js
+++ src/internal/fs.js
@@ -9,19 +9,21 @@ import * as fs from "node:fs";
  * Synchronously look up if the `path` exists.
  *
  * @param {string} path The path to look up.
+ * @param {object} [deps] Node.js' fs module.
  * @returns {boolean} `true` if the path exists, `false` otherwise.
  */
-export function existsSync(path, _fs = fs) {
-  return _fs.existsSync(path);
+export function existsSync(path, deps = fs) {
+  return deps.existsSync(path);
 }
 
 /**
  * Synchronously look up the (target) location of a symbolic link.
  *
  * @param {string} path The path to look up.
+ * @param {object} [deps] Node.js' fs module.
  * @returns {string} The contents of `path`.
  * @throws {Error} If `path` is not a symbolic link.
  */
-export function readlinkSync(path, _fs = fs) {
-  return _fs.readlinkSync(path);
+export function readlinkSync(path, deps = fs) {
+  return deps.readlinkSync(path);
 }
