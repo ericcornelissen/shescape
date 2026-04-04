19cb3d165cc66600ce2c9cfc88512fb3ec61a03a
Eric Cornelissen <ericornelissen@gmail.com>
2026-04-04 12:58:28 +0200

    Differentially test linear-time regexp support for CMD
    Create a unit test that differentially tests the change in CMD escaping
    from "normal" regular expression to linear-time-based regular
    expressions (in [1]). To do this properly, the `shescape-previous`
    version is downgraded to the version prior to this migration.
    
    [1]: 0f38a425b6872a2489fa422ac573ab4d88fd9554

---
+++ test/unit/win/differential.test.js
@@ -0,0 +1,55 @@
+/**
+ * @overview Contains differential tests for the migration form standard regular
+ * expressions to linear-time regular expressions in CMD.
+ * @license MIT
+ */
+
+import { testProp } from "@fast-check/ava";
+import * as fc from "fast-check";
+
+import * as old from "../../../node_modules/shescape-previous/src/internal/win/cmd.js";
+import * as upd from "../../../src/internal/win/cmd.js";
+
+const numRuns = 10_000_000;
+
+testProp(
+  "escape functionality is unchanged",
+  [fc.string()],
+  (t, arg) => {
+    const updFn = upd.getEscapeFunction();
+    const oldFn = old.getEscapeFunction();
+
+    const got = updFn(arg);
+    const want = oldFn(arg);
+    t.is(got, want);
+  },
+  { numRuns },
+);
+
+testProp(
+  "quote functionality is unchanged",
+  [fc.string()],
+  (t, arg) => {
+    const updFn = upd.getQuoteFunction();
+    const oldFn = old.getQuoteFunction();
+
+    const got = updFn[0](updFn[1](arg));
+    const want = oldFn[0](oldFn[1](arg));
+    t.is(got, want);
+  },
+  { numRuns },
+);
+
+testProp(
+  "flag protection functionality is unchanged",
+  [fc.string()],
+  (t, arg) => {
+    const updFn = upd.getFlagProtectionFunction();
+    const oldFn = old.getFlagProtectionFunction();
+
+    const got = updFn(arg);
+    const want = oldFn(arg);
+    t.is(got, want);
+  },
+  { numRuns },
+);
