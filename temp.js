2ffe437576103492ed85e6132eab0c4eeaedfafa
Eric Cornelissen <ericornelissen@gmail.com>
2026-04-05 13:10:33 +0200

    correction

--- temp.js
+++ temp.js
@@ -1,24 +1,24 @@
 import { exec } from "node:child_process";
 import { Shescape } from "shescape";
 
-const options = { shell: 'powershell' };
-const shescape = new Shescape({ shell: 'powershell' });
+const options = { shell: "powershell" };
+const shescape = new Shescape({ shell: "powershell" });
 
-exec(`gh ${shescape.escape("--help")}`, (error) => {
+exec(`gh ${shescape.escape("--help")}`, options, (error) => {
   if (error) {
     console.log("gh --help : error");
   } else {
     console.log("gh --help : no error");
   }
 });
-exec(`gh ${shescape.escape("`--help")}`, (error) => {
+exec(`gh ${shescape.escape("`--help")}`, options, (error) => {
   if (error) {
     console.log("gh `--help : error");
   } else {
     console.log("gh `--help : no error");
   }
 });
-exec(`gh ${shescape.escape("``--help")}`, (error) => {
+exec(`gh ${shescape.escape("``--help")}`, options, options, (error) => {
   if (error) {
     console.log("gh ``--help : error");
   } else {
@@ -26,21 +26,21 @@ exec(`gh ${shescape.escape("``--help")}`, (error) => {
   }
 });
 
-exec(`gh ${shescape.quote("--help")}`, (error) => {
+exec(`gh ${shescape.quote("--help")}`, options, (error) => {
   if (error) {
     console.log("gh '--help': error");
   } else {
     console.log("gh '--help': no error");
   }
 });
-exec(`gh ${shescape.quote("`--help")}`, (error) => {
+exec(`gh ${shescape.quote("`--help")}`, options, (error) => {
   if (error) {
     console.log("gh '`--help': error");
   } else {
     console.log("gh '`--help': no error");
   }
 });
-exec(`gh ${shescape.quote("``--help")}`, (error) => {
+exec(`gh ${shescape.quote("``--help")}`, options, (error) => {
   if (error) {
     console.log("gh '``--help': error");
   } else {
